22f4c61edeb6bd1f2aa572331781cd4bd0c59a07
markohanesian <mso872@gmail.com>
2020-10-27 12:39:27 -0700

    Added dark mode toggle button, heading, subheading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import './App.css';
+import React, { useState } from "react";
+import { Switch, Grid, Typography, Button, Paper } from "@material-ui/core";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+// import './App.css';
 // components
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? "dark" : "light",
+    },
+  });
   return (
     <div className="App">
+      <ThemeProvider theme={theme}>
+        <Paper
+          style={{ height: "100vh", display: "flex", flexDirection: "column", padding: "1rem" }}
+        >
+          <Grid style={{ display: "flex", flexDirection: "row", justifyContent: "flex-end" }}> 
+            <Switch
+              checked={darkMode}
+              onChange={() => setDarkMode(!darkMode)}
+            ></Switch>
+          </Grid>
+          <Grid>
+            <Typography variant="h4" component="h2" gutterBottom>
+              Hello, I'm Mark
+            </Typography>
+          </Grid>
+          <Grid>
+            <Button>button 1</Button>
+          </Grid>
+        </Paper>
+      </ThemeProvider>
     </div>
   );
 }
