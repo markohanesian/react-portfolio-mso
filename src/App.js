4eea19ee955d405de6cc951c573e44e136bf95ca
markohanesian <mso872@gmail.com>
2020-10-27 12:50:20 -0700

    updated paper styling; added page subtitle "Full Stack Dev etc."

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Switch, Grid, Typography, Button, Paper } from "@material-ui/core";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-// import './App.css';
+import "./App.css";
 // components
 
 function App() {
@@ -16,20 +16,34 @@ function App() {
     <div className="App">
       <ThemeProvider theme={theme}>
         <Paper
-          style={{ height: "100vh", display: "flex", flexDirection: "column", padding: "1rem" }}
+          style={{
+            height: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            padding: "1rem",
+          }}
         >
-          <Grid style={{ display: "flex", flexDirection: "row", justifyContent: "flex-end" }}> 
+          <Grid
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-end",
+            }}
+          >
             <Switch
               checked={darkMode}
               onChange={() => setDarkMode(!darkMode)}
             ></Switch>
           </Grid>
-          <Grid>
+          <Grid class="grid-default">
             <Typography variant="h4" component="h2" gutterBottom>
               Hello, I'm Mark
             </Typography>
+            <Typography variant="subtitle1" component="h2" gutterBottom>
+              Full Stack Developer ++ Designer
+            </Typography>
           </Grid>
-          <Grid>
+          <Grid class="grid-default">
             <Button>button 1</Button>
           </Grid>
         </Paper>
