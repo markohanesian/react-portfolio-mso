4499a3bac239dd9d91561b210025fabf0861959d
markohanesian <mso872@gmail.com>
2020-12-22 13:39:05 -0800

    Created Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import { Grid, Typography, CssBaseline, Toolbar } from '@material-ui/core/';
+import IconButton from '@material-ui/core/IconButton';
+// social link icons
+import GitHubIcon from '@material-ui/icons/GitHub';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    text: {
+      padding: theme.spacing(2, 2, 0),
+    },
+    footer: {
+      top: 'auto',
+      bottom: 0,
+      backgroundColor: 'transparent',
+      marginTop: '2rem',
+    },
+    icon: {
+      color: 'black'
+    },
+    grow: {
+      flexGrow: 1,
+    },
+  }),
+);
+
+export default function Footer() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Grid position="relative" color="primary" className={classes.footer}>
+        <Toolbar>
+            <div>
+                <Typography variant="body1" color="black" gutterBottom>
+                    2020 Mark Ohanesian
+                </Typography>
+            </div>
+          <div className={classes.grow} />
+          <IconButton className={classes.icon} href="mailto:mso872@gmail.com">
+            <MailOutlineIcon />
+          </IconButton>
+          <IconButton className={classes.icon} href="https://github.com/markohanesian">
+            <GitHubIcon/>
+          </IconButton>
+          <IconButton className={classes.icon} href="https://github.com/markohanesian/git-cheatsheet-mso">
+            <LinkedInIcon />
+          </IconButton>
+        </Toolbar>
+      </Grid>
+    </React.Fragment>
+  );
+}
