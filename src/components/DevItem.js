10b2aa5ac5b654e20ce2e2f0a4d4da7fa34cc41e
mso872 <mso872@gmail.com>
2024-02-29 06:46:59 -0800

    Conditionally render both site and code buttons

--- src/components/DevItem.js
+++ src/components/DevItem.js
@@ -1,36 +1,35 @@
-import React from 'react';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
+import React from "react";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
-      display: 'flex',
-      flexDirection: 'row',
+      display: "flex",
+      flexDirection: "row",
       // alignSelf: 'flex-start',
-      margin: '1rem',
-      padding: '1rem',
-      maxWidth: '400px'
+      margin: "1rem",
+      padding: "1rem",
+      maxWidth: "400px",
     },
     content: {
-      display: 'flex',
-      flexDirection: 'column',
+      display: "flex",
+      flexDirection: "column",
       // flex: '1 0 auto',
-      padding: '0.5rem',
-      maxWidth: '200px'
+      padding: "0.5rem",
+      maxWidth: "200px",
     },
     imageContainer: {
-      display: 'flex',
-      width: 'auto'
+      display: "flex",
+      width: "auto",
     },
     image: {
       width: 120,
-      marginRight: '1rem'
+      marginRight: "1rem",
     },
     title: {
       fontSize: "1rem",
@@ -39,15 +38,15 @@ const useStyles = makeStyles((theme: Theme) =>
     description: {
       fontSize: ".8rem",
       fontWeight: "400",
-      marginTop: '1rem'
+      marginTop: "1rem",
     },
     buttons: {
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'flex-start',
-      alignItems: 'flex-end',
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "flex-start",
+      alignItems: "flex-end",
     },
-  }),
+  })
 );
 
 export default function MediaControlCard(props) {
@@ -67,12 +66,20 @@ export default function MediaControlCard(props) {
           <Typography className={classes.title} component="h5" variant="h5">
             {props.cardTitle}
           </Typography>
-          <Typography className={classes.description} variant="body2" color="textSecondary">
+          <Typography
+            className={classes.description}
+            variant="body2"
+            color="textSecondary"
+          >
             {props.cardDescription}
           </Typography>
         </CardContent>
         <div className={classes.buttons}>
-          <Button color="secondary" href={props.deployURL} target="blank">Site</Button>
+          {props.deployURL && (
+            <Button color="secondary" href={props.deployURL} target="blank">
+              Site
+            </Button>
+          )}
           {props.codeURL && (
             <Button color="secondary" href={props.codeURL} target="blank">
               Code
