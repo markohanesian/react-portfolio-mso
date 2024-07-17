e1c8cbbc653e17c0c3d19de3933ff4dfb33a3d4f
mso872 <mso872@gmail.com>
2024-07-17 15:29:49 -0700

    Fix <p> nesting error again

--- src/components/CurrentYear.js
+++ src/components/CurrentYear.js
@@ -1,19 +1,13 @@
 import React from "react";
 import { Typography } from "@mui/material";
 
-const CurrentYearStyle = {
-  fontSize: "1rem",
-};
-
 export default function CurrentYear() {
   const today = new Date();
   var year = today.getFullYear();
 
   return (
-    <span style={CurrentYearStyle}>
-      <Typography variant="body1" color="textSecondary">
+      <Typography component='span' variant="body1" color="textSecondary">
         {year} Mark Ohanesian
       </Typography>
-    </span>
   );
 }
