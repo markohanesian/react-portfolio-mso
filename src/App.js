f59cf059f95d60db6253ab781ade3915df586ae2
mso872 <mso872@gmail.com>
2024-07-17 14:48:02 -0700

    Fix theme switcher

--- src/App.js
+++ src/App.js
@@ -1,174 +1,175 @@
-import React, { useState } from "react";
-import { ThemeProvider, useTheme, makeStyles } from "@mui/styles";
+import React, { useState, useMemo } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Grid, Paper, Stack } from "@mui/material";
 import DarkModeToggle from "react-dark-mode-toggle";
 import "./App.css";
 // components
 import NavBar from "./components/NavBar";
-import Header from "../src/components/Header";
-import Footer from "../src/components/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 // Development items
 import DevItem from "./components/DevItem";
 // Design Items
 import DesignItem from "./components/DesignItem";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  app: {
+    // height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    padding: "1rem",
+  },
+  topBar: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  heading2: {
+    display: "flex",
+    flexDirection: "row",
+    alignSelf: "center",
+    fontSize: "1.3rem",
+    fontWeight: "300",
+    paddingBottom: "0",
+  },
+  subtitle: {
+    fontSize: "0.8rem",
+    fontWeight: "400",
+  },
+  bottomNav: {
+    backgroundColor: "green",
+  },
+  gridContainer: {
+    flexGrow: 1,
+    marginTop: "1rem",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  gridColumn: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "1rem",
+  },
+  DevContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  SectionIcon: {
+    margin: "2rem 0 0 0",
+    fontSize: "3rem",
+  },
+});
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
-  const theme = useTheme({
+  const theme = useMemo(() => createTheme({
     palette: {
-      type: darkMode ? "dark" : "light",
+      mode: darkMode ? "dark" : "light",
     },
-  });
-  const useStyles = makeStyles({
-    root: {
-      flexGrow: 1,
-    },
-    app: {
-      // height: "100vh",
-      display: "flex",
-      flexDirection: "column",
-      padding: "1rem",
-    },
-    topBar: {
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "flex-end",
-      alignItems: "center",
-    },
-    heading2: {
-      display: "flex",
-      flexDirection: "row",
-      alignSelf: "center",
-      fontSize: "1.3rem",
-      fontWeight: "300",
-      paddingBottom: "0",
-    },
-    subtitle: {
-      fontSize: "0.8rem",
-      fontWeight: "400",
-    },
-    bottomNav: {
-      Backgroundcolor: "green",
-    },
-    gridContainer: {
-      flexGrow: 1,
-      marginTop: "1rem",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-    },
-    gridColumn: {
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      padding: "1rem",
-    },
-    DevContainer: {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-    },
-    SectionIcon: {
-      margin: "2rem 0 0 0",
-      fontSize: "3rem",
-    },
-  });
+  }), [darkMode]);
+
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <ThemeProvider theme={theme}>
-        <Paper className={classes.app}>
-          <Grid className={classes.topBar}>
-            <DarkModeToggle
-              size={60}
-              checked={darkMode}
-              onChange={() => setDarkMode(!darkMode)}
-            ></DarkModeToggle>
-          </Grid>
-          <Header />
-          <Router>
-            <div>
-              <Switch>
-                <Route
-                  path="/photography"
-                  component={() => {
-                    window.location.href = "http://markohanesian.photography/";
-                    return null;
-                  }}
-                />
-                {/* Design Section */}
-                <Route path="/design">
-                  <Stack className={classes.gridContainer} container>
-                    <Stack className={classes.DevContainer}>
-                      <NavBar />
-                      <DesignItem
-                        cardImage="item-images/styledPP.png"
-                        cardTitle="Web App Dashboard Concept"
-                        cardDescription="Wardrobe app concept user Dashboard designed in Adobe xD with original photography"
-                        dribbleURL="https://dribbble.com/shots/13904483-Profile-Page-styld-app-concept"
-                      />
-                      <DesignItem
-                        cardImage="item-images/tulips-concept.png"
-                        cardTitle="Shoe Product Page Concept"
-                        cardDescription="Product landing page concept made in Adobe XD | featuring my product photography"
-                        dribbleURL="https://dribbble.com/shots/14062665-Tulips-homepage-concept"
-                      />
+    <ThemeProvider theme={theme}>
+      <Paper className={classes.app}>
+        <Grid className={classes.topBar}>
+          <DarkModeToggle
+            size={60}
+            checked={darkMode}
+            onChange={() => setDarkMode(!darkMode)}
+          />
+        </Grid>
+        <Header />
+        <Router>
+          <div>
+            <Switch>
+              <Route
+                path="/photography"
+                component={() => {
+                  window.location.href = "http://markohanesian.photography/";
+                  return null;
+                }}
+              />
+              {/* Design Section */}
+              <Route path="/design">
+                <Stack className={classes.gridContainer} container>
+                  <Stack className={classes.DevContainer}>
+                    <NavBar />
+                    <DesignItem
+                      cardImage="item-images/styledPP.png"
+                      cardTitle="Web App Dashboard Concept"
+                      cardDescription="Wardrobe app concept user Dashboard designed in Adobe xD with original photography"
+                      dribbleURL="https://dribbble.com/shots/13904483-Profile-Page-styld-app-concept"
+                    />
+                    <DesignItem
+                      cardImage="item-images/tulips-concept.png"
+                      cardTitle="Shoe Product Page Concept"
+                      cardDescription="Product landing page concept made in Adobe XD | featuring my product photography"
+                      dribbleURL="https://dribbble.com/shots/14062665-Tulips-homepage-concept"
+                    />
 
-                      <DesignItem
-                        cardImage="item-images/hisherswatches.jpg"
-                        cardTitle="Apple Watch Faces Concept"
-                        cardDescription="Apple Watch faces concept designed in Adobe xD and Photoshop"
-                        dribbleURL="https://dribbble.com/shots/13774573-Apple-Watch-Face-Concept-Rose-Gold-His-Hers?utm_source=Clipboard_Shot&utm_campaign=markohanesian&utm_content=Apple%20Watch%20Face%20Concept%20-%20Rose%20Gold%20His%20%2B%20Hers&utm_medium=Social_Share"
-                      />
+                    <DesignItem
+                      cardImage="item-images/hisherswatches.jpg"
+                      cardTitle="Apple Watch Faces Concept"
+                      cardDescription="Apple Watch faces concept designed in Adobe xD and Photoshop"
+                      dribbleURL="https://dribbble.com/shots/13774573-Apple-Watch-Face-Concept-Rose-Gold-His-Hers?utm_source=Clipboard_Shot&utm_campaign=markohanesian&utm_content=Apple%20Watch%20Face%20Concept%20-%20Rose%20Gold%20His%20%2B%20Hers&utm_medium=Social_Share"
+                    />
 
-                      <DesignItem
-                        cardImage="item-images/signup-pgp.png"
-                        cardTitle="App Signup Page Concept"
-                        cardDescription="Mobile-friendly signup form conceptdesigned in Adobe xD"
-                        dribbleURL="https://dribbble.com/shots/13495221-Signup-Form-Concept-Project-Golden-Phoenix?utm_source=Clipboard_Shot&utm_campaign=markohanesian&utm_content=Signup%20Form%20Concept%20-%20Project%20Golden%20Phoenix&utm_medium=Social_Share"
-                      />
-                    </Stack>
+                    <DesignItem
+                      cardImage="item-images/signup-pgp.png"
+                      cardTitle="App Signup Page Concept"
+                      cardDescription="Mobile-friendly signup form conceptdesigned in Adobe xD"
+                      dribbleURL="https://dribbble.com/shots/13495221-Signup-Form-Concept-Project-Golden-Phoenix?utm_source=Clipboard_Shot&utm_campaign=markohanesian&utm_content=Signup%20Form%20Concept%20-%20Project%20Golden%20Phoenix&utm_medium=Social_Share"
+                    />
                   </Stack>
-                </Route>
-                {/* Development Section */}
-                <Route path="/">
-                  <Grid className={classes.gridContainer} container>
-                    <Stack className={classes.DevContainer}>
-                      <NavBar />
-                      <DevItem
-                        cardImage="item-images/react-social-preview.png"
-                        cardTitle="ReactSocial"
-                        cardDescription="A social media app featuring the ability to post text and images, and comment on user posts. Built with reactjs, firebase, and materialui"
-                        deployURL="https://social-media-app-mso.web.app/"
-                        codeURL="https://github.com/markohanesian/ReactSocial"
-                      />
-                      <DevItem
-                        cardImage="item-images/git-good.png"
-                        cardTitle="Git Good"
-                        cardDescription="Git language cheatsheet with copy to clipboard. Built with ReactJS and features a custom logo design"
-                        deployURL="https://git-cheatsheet-mso.netlify.app/"
-                        codeURL="https://github.com/markohanesian/git-cheatsheet-mso"
-                      />
-                      {/* <DevItem
-                        cardImage="item-images/former-mso.png"
-                        cardTitle="Former"
-                        cardDescription="Form creator website with export to pdf"
-                        deployURL="https://former-mso.netlify.app/"
-                        codeURL="https://github.com/markohanesian/former-mso"
-                      /> */}
-                    </Stack>
-                  </Grid>
-                </Route>
-              </Switch>
-            </div>
-          </Router>
-          <Footer />
-        </Paper>
-      </ThemeProvider>
-    </div>
+                </Stack>
+              </Route>
+              {/* Development Section */}
+              <Route path="/">
+                <Grid className={classes.gridContainer} container>
+                  <Stack className={classes.DevContainer}>
+                    <NavBar />
+                    <DevItem
+                      cardImage="item-images/react-social-preview.png"
+                      cardTitle="ReactSocial"
+                      cardDescription="A social media app featuring the ability to post text and images, and comment on user posts. Built with reactjs, firebase, and materialui"
+                      deployURL="https://social-media-app-mso.web.app/"
+                      codeURL="https://github.com/markohanesian/ReactSocial"
+                    />
+                    <DevItem
+                      cardImage="item-images/git-good.png"
+                      cardTitle="Git Good"
+                      cardDescription="Git language cheatsheet with copy to clipboard. Built with ReactJS and features a custom logo design"
+                      deployURL="https://git-cheatsheet-mso.netlify.app/"
+                      codeURL="https://github.com/markohanesian/git-cheatsheet-mso"
+                    />
+                    {/* <DevItem
+                      cardImage="item-images/former-mso.png"
+                      cardTitle="Former"
+                      cardDescription="Form creator website with export to pdf"
+                      deployURL="https://former-mso.netlify.app/"
+                      codeURL="https://github.com/markohanesian/former-mso"
+                    /> */}
+                  </Stack>
+                </Grid>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+        <Footer />
+      </Paper>
+    </ThemeProvider>
   );
 }
 
