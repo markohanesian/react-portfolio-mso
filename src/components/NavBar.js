9e35b8dd06268b0f81177ad7f35a70822d6d1a5d
mso872 <mso872@gmail.com>
2024-07-17 08:06:03 -0700

    Fix navbar to display current page active

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@mui/styles';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import CodeIcon from '@mui/icons-material/Code';
 import BrushIcon from '@mui/icons-material/Brush';
 // import CameraAlt from '@mui/icons-material/CameraAlt';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const useStyles = makeStyles({
   root: {
@@ -21,23 +21,32 @@ const useStyles = makeStyles({
 
 export default function SimpleBottomNavigation() {
   const classes = useStyles();
-    const [value, setValue] = React.useState(0);
-    const history = useHistory();
+  const [value, setValue] = React.useState('home');
+  const history = useHistory();
+  const location = useLocation();
 
-    const handleChange = (event, newValue) => {
-      history.push(`/${newValue}`);
-      setValue(newValue);
-    };
-    
+  useEffect(() => {
+    if (location.pathname === '/design') {
+      setValue('design');
+    } else {
+      setValue('home');
+    }
+  }, [location.pathname]);
+
+  const handleChange = (event, newValue) => {
+    history.push(`/${newValue}`);
+    setValue(newValue);
+  };
+
   return (
     <BottomNavigation
       value={value}
-      onChange={handleChange} 
+      onChange={handleChange}
       showLabels
       className={classes.root}
     >
-      <BottomNavigationAction className={classes.NavButtons} value="home"  label="Development" icon={<CodeIcon />} />
-      <BottomNavigationAction className={classes.NavButtons} value="design"  label="Design" icon={<BrushIcon />} />
+      <BottomNavigationAction className={classes.NavButtons} value="home" label="Development" icon={<CodeIcon />} />
+      <BottomNavigationAction className={classes.NavButtons} value="design" label="Design" icon={<BrushIcon />} />
       {/* <BottomNavigationAction className={classes.NavButtons} value="photography" label="Photography" icon={<CameraAlt />} /> */}
     </BottomNavigation>
   );
