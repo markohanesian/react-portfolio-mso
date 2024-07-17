bff2327725b77a68f95c792987e6a66b66137004
mso872 <mso872@gmail.com>
2024-07-17 07:57:13 -0700

    Comment out button import to fix no unused vars

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { makeStyles } from "@mui/styles";
 import Typography from "@mui/material/Typography";
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 
 const useStyles = makeStyles({
   image: {
