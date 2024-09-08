d3e48ff54c5d5d8f31efeb61ec49df6cc167e5c5
mso872 <mso872@gmail.com>
2024-09-07 17:55:28 -0700

    Fix darkmode switch not rendering on mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,24 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import HomeIcon from '@mui/icons-material/Home';
-import { useNavigate } from 'react-router-dom';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import HomeIcon from "@mui/icons-material/Home";
+import { useNavigate } from "react-router-dom";
 
 const dropdownPages = [
-  { name: 'Design', route: '/design' },
-  { name: 'Development', route: '/development' },
-  { name: 'Photography', route: '/photography' },
-  { name: 'Case Study', route: '/react-social-case-study' }
+  { name: "Design", route: "/design" },
+  { name: "Development", route: "/development" },
+  { name: "Photography", route: "/photography" },
+  { name: "Case Study", route: "/react-social-case-study" },
 ];
-const settings = ['Design', 'Development'];
+const settings = ["Design", "Development"];
 
 function ResponsiveAppBar(props) {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -42,18 +42,24 @@ function ResponsiveAppBar(props) {
           {/* Home icon on the left */}
           <IconButton
             aria-label="home"
-            onClick={() => navigate('/')}
+            onClick={() => navigate("/")}
             color="inherit"
-            sx={{ display: { xs: 'flex', md: 'flex' }, mr: 1 }}
+            sx={{ display: { xs: "flex", md: "flex" }, mr: 1 }}
           >
-            <HomeIcon sx={{height: "32px", width: "32px"}} />
+            <HomeIcon sx={{ height: "32px", width: "32px" }} />
           </IconButton>
 
-
-
           {/* Right side avatar and menu */}
-                    {/* Menu button on mobile */}
-                    <Box sx={{ justifyContent: "flex-end", flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          {/* Menu button on mobile */}
+          <Box
+            sx={{
+              justifyContent: "flex-end",
+              alignItems: "center",
+              flexGrow: 1,
+              display: { xs: "flex", md: "none" },
+            }}
+          >
+            {props.children}
             <IconButton
               size="large"
               aria-label="menu"
@@ -68,40 +74,64 @@ function ResponsiveAppBar(props) {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={() => handleCloseNavMenu(null)}
-              sx={{ display: { xs: 'block', md: 'none' } }}
+              sx={{ display: { xs: "block", md: "none" } }}
             >
               {dropdownPages.map((page) => (
-                <MenuItem key={page.name} onClick={() => handleCloseNavMenu(page.route)}>
-                  <Typography sx={{ textAlign: 'center', fontFamily: 'Marcellus, serif', textTransform: "none", fontSize: "1.5rem" }}>{page.name}</Typography>
+                <MenuItem
+                  key={page.name}
+                  onClick={() => handleCloseNavMenu(page.route)}
+                >
+                  <Typography
+                    sx={{
+                      textAlign: "center",
+                      fontFamily: "Marcellus, serif",
+                      textTransform: "none",
+                      fontSize: "1.5rem",
+                    }}
+                  >
+                    {page.name}
+                  </Typography>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
 
           {/* Menu button on larger screens */}
-          <Box sx={{ justifyContent: "flex-end", flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {dropdownPages.filter(page => settings.includes(page.name)).map((page) => (
-              <Button
-                key={page.name}
-                onClick={() => handleCloseNavMenu(page.route)}
-                sx={{ my: 2, color: 'white', display: 'block', fontFamily: 'Marcellus, serif', textTransform: "none", fontSize: "1.5rem"  }}
-              >
-                {page.name}
-              </Button>
-            ))}
-          </Box>
-          <Box sx={{ justifyContent: "flex-end", flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {props.children}
+          <Box
+            sx={{
+              justifyContent: "flex-end",
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+            }}
+          >
+            {dropdownPages
+              .filter((page) => settings.includes(page.name))
+              .map((page) => (
+                <Button
+                  key={page.name}
+                  onClick={() => handleCloseNavMenu(page.route)}
+                  sx={{
+                    my: 2,
+                    color: "white",
+                    display: "block",
+                    fontFamily: "Marcellus, serif",
+                    textTransform: "none",
+                    fontSize: "1.5rem",
+                  }}
+                >
+                  {page.name}
+                </Button>
+              ))}
           </Box>
         </Toolbar>
       </Container>
